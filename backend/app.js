1f492258e51756e6574f150813cb9360405dc81d
Saron <saronabebe2019@gmail.com>
2025-10-29 17:50:41 -0800

    Set up frontend structure

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,51 @@
+const bcrypt = require("bcryptjs");
+
 require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+// Step 1: Initialize passport
+passport.use(
+  session({ secret: "cats", resave: false, saveUninitialized: false })
+);
+passport.initialize(); // not mandatory
+passport.session();
 
 const PORT = process.env.PORT || 3000;
 
+// define ejs -view engine and where to find the views
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+
+// define access path for static files
+const accessPath = path.join(__dirname, "./public");
+app.use(express.static(accessPath));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //parse form data
+
+//Step 2: define strategy
+passport.use(
+  new localStrategy(async (username, password, done) => {
+    try {
+      //find user from db (check and authenticate)
+    } catch (err) {}
+  })
+);
+
+//Step 3: Serialize and deserialize user
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser(async (id, done) => {
+  try {
+    // return the user from db using the id
+  } catch (err) {}
+});
 //routes
 
 app.listen(PORT, (err) => {
---
+++ front-end/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      {/* //header component
+            //banner component
+            //product listing component
+            //footer component */}
+    </div>
+  );
+};
+
+export default Homepage;
